d25e6d296c9bfb4c0e83171eeb5a6d75666d8b18
Yoshiki Takashima <ytakashi@andrew.cmu.edu>
2023-01-06 22:37:12 -0500

    Fix review comments. (#4)
    * Fixed comments that were wrong/lacked detail.
    
    * Purged expected file since we are not running that.

--- tests/cargo-kani/proptest-lib/src/lib.rs
+++ tests/cargo-kani/proptest-lib/src/lib.rs
@@ -1,7 +1,8 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! basic proptest examples. First one fails but second does not.
+//! Basic proptest examples. First one verifies, but the second one
+//! does not.
 
 #[cfg(test)]
 mod test {
@@ -17,7 +18,8 @@ mod test {
     mod expected {
         proptest! {
             /// Overflow by check in x * 2. TODO: Need to implement
-            /// expected fail tests. tracking issue #5
+            /// expected fail tests. tracking issue:
+            /// https://github.com/model-checking/kani-github-action/issues/5
             fn fails(even_num in any::<i32>().prop_map(|x| x * 2)) {
                 prop_assert_eq!(even_num % 2, 0, "even number");
             }
