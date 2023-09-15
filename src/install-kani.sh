a89f30c42e50147c8e738368c10878ad79e2a695
jaisnan <jaisnan@amazon.com>
2023-09-14 21:35:31 -0400

    Verify version number of Kani installed

--- src/install-kani.sh
+++ src/install-kani.sh
@@ -12,9 +12,26 @@ fi
 
 # Check exit status for error handling
 if [ $? -ne O ]; then
-    echo "Error installing Kani. Please check if the provided version is correct"
+    echo "::error::Could not install Kani. Please check if the provided version is correct"
     exit 1
 fi
 
 # Setup kani in ci
 cargo-kani setup;
+
+# Get the current installed version of kani and check it against the latest version
+installed_version=$(kani --version | awk '{print $2}')
+
+if [ $? -eq 0 ]; then
+    if [ "$1" == "latest" ]; then
+        # Cargo search returns version number as string
+        requested_version=$(cargo search kani-verifier | grep -m 1 "^kani-verifier " | awk '{print $3}')
+    else
+        requested_version=$1
+    fi
+
+    if ["$installed_version" != "$requested_version"]; then
+        echo "::error::The version of Kani installed was different than the one requested"
+        exit 1
+    fi
+fi
